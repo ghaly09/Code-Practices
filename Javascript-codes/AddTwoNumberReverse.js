56d322931c2eb8a03ba5250f4a5cb03c4f3cf576
ghaly09 <rachmatghaly0997@gmail.com>
2023-04-05 12:51:53 +0700

    code: Finish LongestSuvstring.js Problem

--- Javascript-codes/AddTwoNumberReverse.js
+++ Javascript-codes/AddTwoNumberReverse.js
@@ -1,5 +1,5 @@
 /**
- * Two Sum Checker
+ * Add Two Number Reverse
  * ===============
 You are given two non-empty linked lists representing two non-negative integers. The digits are stored in reverse order, and each of their nodes contains a single digit. Add the two numbers and return the sum as a linked list.
 You may assume the two numbers do not contain any leading zero, except the number 0 itself.
@@ -20,7 +20,7 @@ var addTwoNumbers = function (l1, l2) {
 };
 
 // Test Run
-addTwoNumbers([2, 4, 3], [5, 6, 4]);
+console.log(addTwoNumbers([2, 4, 3], [5, 6, 4]));
 console.log(addTwoNumbers([0], [0]));
 console.log(addTwoNumbers([9, 9, 9, 9, 9, 9, 9], [9, 9, 9, 9]));
 
---
+++ Javascript-codes/LongestSubstring.js
@@ -0,0 +1,62 @@
+/**
+ * Longest Substring Without Repeating Characters
+ * ===============
+Given a string s, find the length of the longest substring without repeating characters.
+*/
+// Code by Rachmat Ghaly
+
+var lengthOfLongestSubstring = function (s) {
+  let result = {};
+  let sArray = s.split("");
+  if (s.length == 1 && s != "") {
+    return (result = 1);
+  } else if (s == "") {
+    return (result = 0);
+  } else {
+    sArray.filter((value, index) => {
+      let sumValue = 0;
+      for (let i = 0; i < sArray.length; i++) {
+        value == sArray[i] ? sumValue++ : false;
+      }
+
+      if (value != sArray[0]) {
+        result[value] = sumValue;
+      } else if ((value = sArray[0])) {
+        for (let i = index + 1; i < sArray.length; i++) {
+          value == sArray[i] ? (result[value] = sumValue) : false;
+        }
+      }
+      //   result[value] = sumValue;
+      // console.log(sumValue);
+    });
+    let finalResult = Object.keys(result).length;
+    return finalResult;
+  }
+};
+
+console.log(lengthOfLongestSubstring("abcabcbb"));
+console.log(lengthOfLongestSubstring("bbbbb"));
+console.log(lengthOfLongestSubstring("pwwkew"));
+console.log(lengthOfLongestSubstring(" "));
+console.log(lengthOfLongestSubstring(""));
+console.log(lengthOfLongestSubstring("aab"));
+
+/**
+ * OUTPUT EXAMPLE
+ * 1
+Input: s = "abcabcbb"
+Output: 3
+Explanation: The answer is "abc", with the length of 3.
+============
+ * 2
+Input: s = "bbbbb"
+Output: 1
+Explanation: The answer is "b", with the length of 1.
+
+============
+ * 3
+Input: s = "pwwkew"
+Output: 3
+Explanation: The answer is "wke", with the length of 3.
+Notice that the answer must be a substring, "pwke" is a subsequence and not a substring.
+ */
