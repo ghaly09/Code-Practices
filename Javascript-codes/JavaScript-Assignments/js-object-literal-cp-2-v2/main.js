8b091bbf26b503b6f295dcdbc50694a28de33869
ghaly09 <rachmatghaly0997@gmail.com>
2023-05-29 23:53:25 +0700

    Code: Solve Objet Literal problem question 02

---
+++ Javascript-codes/JavaScript-Assignments/js-object-literal-cp-2-v2/main.js
@@ -0,0 +1,49 @@
+function stockOpname(items) {
+  let result = {};
+  let itemsObject = {};
+  let stockItems = {
+    Ram: 10,
+    Motherboard: 4,
+    Processor: 5,
+    SSD: 8,
+    PSU: 12,
+    Cooling: 5,
+  };
+  if (items == false) {
+    return "Data not found";
+  } else if (items != undefined) {
+    items.filter((itemType) => {
+      itemsObject[itemType[0]] = itemType[1];
+    });
+
+    for (let key in stockItems) {
+      result[key] = stockItems[key] - itemsObject[key];
+    }
+  }
+
+  return result;
+}
+
+let items1 = [
+  ["PSU", 6],
+  ["SSD", 3],
+  ["Motherboard", 3],
+  ["Ram", 2],
+  ["Cooling", 4],
+  ["Processor", 5],
+];
+
+let items2 = [
+  ["SSD", 5],
+  ["Motherboard", 4],
+  ["Processor", 1],
+  ["PSU", 3],
+  ["Cooling", 5],
+  ["Ram", 5],
+];
+
+console.log(stockOpname(items1));
+console.log(stockOpname(items2));
+console.log(stockOpname([]));
+
+module.exports = stockOpname;
